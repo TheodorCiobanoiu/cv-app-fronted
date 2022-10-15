f1ce6473d808785f21bc5cc9bb3df6389cb11352
Vespan24 <viesperock@gmail.com>
2022-10-15 21:49:56 +0300

    Vespi

--- src/pages/login.js
+++ src/pages/login.js
@@ -20,20 +20,6 @@ import {withRouter} from "../common/with-router";
 
 
 function Copyright(props) {
-<<<<<<< HEAD
-  return (
-      <Typography
-          variant="body2"
-          color="text.secondary"
-          align="center"
-          {...props}
-      >
-        {"Copyright © "}
-        Your Website &nbsp;
-        {new Date().getFullYear()}
-      </Typography>
-  );
-=======
     return (
         <Typography
             variant="body2"
@@ -46,161 +32,12 @@ function Copyright(props) {
             {new Date().getFullYear()}
         </Typography>
     );
->>>>>>> 24d11322ddbcf2b70b6323c55b0cb7e762afd712
 }
 
 const theme = createTheme();
 
 class Login extends Component {
 
-<<<<<<< HEAD
-  constructor(props) {
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.onChangeUsername = this.onChangeUsername.bind(this);
-    this.onChangePassword = this.onChangePassword.bind(this);
-
-    this.state = {
-      username: "",
-      password: "",
-      loading: false,
-      message: ""
-    };
-  }
-
-  onChangeUsername(e) {
-    this.setState({
-      username: e.target.value
-    });
-  }
-
-  onChangePassword(e) {
-    this.setState({
-      password: e.target.value
-    });
-  }
-
-
-  handleSubmit(event) {
-    event.preventDefault();
-    console.log({
-      username: this.state.username,
-      password: this.state.password
-    });
-    this.setState({
-      message: "",
-      loading: true
-    });
-
-    //Nu stiu daca e necesara asta, daca pica ceva se poate scoate
-    // this.form.validateAll();
-
-    AuthService.login(this.state.username, this.state.password).then(
-        () => {
-          this.props.router.navigate("/content");
-          window.location.reload();
-        },
-        error => {
-          const responseMessage = (
-                  error.response &&
-                  error.response.data &&
-                  error.response.data.message) ||
-              error.message ||
-              error.toString();
-
-          this.setState({
-            loading: false,
-            message: responseMessage
-          });
-        }
-    )
-
-
-  };
-
-  render() {
-    return (
-
-        <ThemeProvider theme={
-          theme
-        }
-
-        >
-          <Container component="main" maxWidth="xs">
-            <CssBaseline/>
-            <Box
-                sx={{
-                  marginTop: 8,
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                }}
-            >
-              <Avatar sx={{m: 1, bgcolor: "secondary.main"}}>
-                <LockOutlinedIcon/>
-              </Avatar>
-              <Typography component="h1" variant="h5">
-                Sign in
-              </Typography>
-              <Box
-                  component="form"
-                  onSubmit={this.handleSubmit}
-                  noValidate
-                  sx={{mt: 1}}
-              >
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="username"
-                    label="Username"
-                    name="username"
-                    autoComplete="username"
-                    autoFocus
-                    onChange={this.onChangeUsername}
-                />
-                <TextField
-                    input="passwordField"
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                    onChange={this.onChangePassword}
-                />
-                <FormControlLabel
-                    control={<Checkbox value="remember" color="primary"/>}
-                    label="Remember me"
-                />
-                <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    sx={{mt: 3, mb: 2}}
-                    onClick={this.handleSubmit}
-                >
-                  Sign In
-                </Button>
-                <Grid container>
-                  <Grid item xs>
-                    <Link href="#" variant="body2">
-                      Forgot password?
-                    </Link>
-                  </Grid>
-                </Grid>
-              </Box>
-            </Box>
-            <Copyright sx={{mt: 12, mb: 4}}/>
-          </Container>
-        </ThemeProvider>
-    );
-  }
-}
-export default withRouter(Login);
-=======
     constructor(props) {
         super(props);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -244,7 +81,7 @@ export default withRouter(Login);
 
         AuthService.login(this.state.username, this.state.password).then(
             () => {
-                this.props.router.navigate("/home");
+                this.props.router.navigate("/content");
                 window.location.reload();
             },
             error => {
@@ -347,4 +184,3 @@ export default withRouter(Login);
     }
 }
 export default withRouter(Login);
->>>>>>> 24d11322ddbcf2b70b6323c55b0cb7e762afd712
